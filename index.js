734aebcf8366e9888609e0465dc79ffff649b2a8
tarekul42 <tarekulrifat205@gmail.com>
2023-11-08 23:30:23 -0800

    delete functionality added

--- index.js
+++ index.js
@@ -69,6 +69,13 @@ async function run() {
       res.send(result)
     })
 
+    app.delete('/coffees/:id', async(req, res) =>{
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await espressoEmporiumCoffees.deleteOne(query);
+      res.send(result)
+    })
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
